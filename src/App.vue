bb60909dbd2faef27b215d7f91eab300f486ca77
mmrmilon14 <mmrmilon14@gmail.com>
2023-07-20 20:48:11 +0600

    Episode 01 Code

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <script setup>
-const items = [1,2,3,4,5]
+const tasks = [
+  "Complete project proposal",
+  "Update website content",
+  "Fix bug in user authentication",
+  "Prepare presentation slides",
+  "Test new feature implementation",
+];
 </script>
 
 <template>
-  <section class="mx-auto container flex items-center text-center flex-col space-y-2">
-        <!-- <div class="p-5 border border-gray-600 w-40" v-for="n in 5"> {{ n }}</div> -->
-        <div class="p-5 border border-gray-600 w-40" v-for="item in items"> {{ item }}</div>
-
+  <section class="mx-auto container">
+    <section class="mx-auto container space-y-3">
+        <div class="p-5 border border-gray-600 text-left" v-for="(task, index) in tasks">{{ index+1 }}. {{task}}</div>
     </section>
+  </section>
 </template>
 
 <style scoped>
